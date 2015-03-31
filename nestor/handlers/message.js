e3622b3a29f9b08f84b08da54fe17f52510c596a
Marshall Bowers <elliott.codes@gmail.com>
2015-03-31 16:25:10 -0400

    Respond to simple greetings

--- nestor/handlers/message.js
+++ nestor/handlers/message.js
@@ -1,16 +1,22 @@
 'use strict';
 
+var util = require('util');
+
 var chat = require('../api/chat');
 
 var handleMessage = function (message) {
+
+    console.log(message);
 
     switch (message.type) {
         case 'message':
             if (message.text) {
                 if (/@?nestor/gi.test(message.text)) {
-                    chat.postMessage(message.channel, 'Hello, World!', {
-                        as_user: true
-                    });
+                    if (/hello|hi|xoka/i.test(message.text)) {
+                        chat.postMessage(message.channel, util.format('Good day, <@%s>!', message.user), {
+                            as_user: true
+                        });
+                    }
                 }
             }
             break;
